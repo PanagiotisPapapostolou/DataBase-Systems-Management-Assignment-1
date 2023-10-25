fcecdaa0bc69411f67449831834c672869de66f1
AntonisZks <antoniszks2003@gmail.com>
2023-10-25 23:13:41 +0300

    Started the HP_CreateFile() function

--- src/hp_file.c
+++ src/hp_file.c
@@ -11,28 +11,39 @@
   BF_ErrorCode code = call; \
   if (code != BF_OK) {         \
     BF_PrintError(code);    \
-    return HP_ERROR;        \
+    exit(code);        \
   }                         \
 }
 
 int HP_CreateFile(char *fileName){
-    return 0;
+  int fileDescriptor;
+  void* data;
+  BF_Block* block;
+  BF_Block_Init(&block);
+
+  CALL_BF(BF_CreateFile(fileName));
+  CALL_BF(BF_OpenFile(fileName, &fileDescriptor));
+
+  CALL_BF(BF_AllocateBlock(fileDescriptor, block));
+
+  free(block);
+  return 0;
 }
 
 HP_info* HP_OpenFile(char *fileName){
-    return NULL ;
+  return NULL ;
 }
 
 
 int HP_CloseFile( HP_info* hp_info ){
-    return 0;
+  return 0;
 }
 
 int HP_InsertEntry(HP_info* hp_info, Record record){
-    return 0;
+  return 0;
 }
 
 int HP_GetAllEntries(HP_info* hp_info, int value){
-   return 0;
+  return 0;
 }
 
